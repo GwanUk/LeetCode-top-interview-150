75b82885d61fe63f0a6c2c8806f58d1af8cc946b
gwanuk <guk1502012@gmail.com>
2023-09-02 16:46:36 +0900

    파일 정리

--- src/evaluate_reverse_polish_notation_50/Main.java
+++
@@ -1,8 +0,0 @@
-package evaluate_reverse_polish_notation_50;
-
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int i = solution.evalRPN(new String[]{"4", "13", "5", "/", "+"});
-    }
-}
